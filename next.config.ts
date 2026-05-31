1be3c58cc0eff1d768967998706127752f490346
tobiasfremming <tobias.fremming@gmail.com>
2026-05-31 18:27:57 +0200

    feat: add base path support and update peer dependencies in Dockerfile and next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from 'next';
 
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
 const nextConfig = {
+  basePath,
   output: 'standalone',
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
